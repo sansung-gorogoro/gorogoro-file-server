a6c02a10f06d2a9e969aff21cc6abeb4be8de8fb
juyeongMoon888 <whoor2828@gmail.com>
2025-12-28 21:47:44 +0900

    chore: JPA Auditing과 ConfigurationProperties 활성화

--- src/main/java/com/sansung_gorogoro/file_server/FileServerApplication.java
+++ src/main/java/com/sansung_gorogoro/file_server/FileServerApplication.java
@@ -1,13 +1,17 @@
 package com.sansung_gorogoro.file_server;
 
+import com.sansung_gorogoro.file_server.infrastructure.config.UploadProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
+@EnableConfigurationProperties(UploadProperties.class)
 @SpringBootApplication
 public class FileServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FileServerApplication.class, args);
 	}
-
 }
