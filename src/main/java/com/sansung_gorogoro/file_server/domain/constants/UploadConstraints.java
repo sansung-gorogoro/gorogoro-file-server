4216d9a69d6be2e15640a4c02a150beefca2f5ec
juyeongMoon888 <whoor2828@gmail.com>
2026-01-07 15:38:23 +0900

    [File] 업로드 정책 상수화로 하드코딩 제거

---
+++ src/main/java/com/sansung_gorogoro/file_server/domain/constants/UploadConstraints.java
@@ -0,0 +1,6 @@
+package com.sansung_gorogoro.file_server.domain.constants;
+
+public class UploadConstraints {
+    private UploadConstraints() {}
+    public static final int ORIGINAL_FILE_NAME_MAX_LEN = 255;
+}
